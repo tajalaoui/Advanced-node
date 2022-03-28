838cffcdf2044db5eb4ad992ea5686403f35b8c8
Tajeddine <tajeddine.js@hotmail.com>
2022-03-28 15:24:53 +0000

    separation of concerns: added seperate file for routes and auth

--- server.js
+++ server.js
@@ -6,18 +6,8 @@ const myDB = require("./connection")
 const fccTesting = require("./freeCodeCamp/fcctesting.js")
 const session = require("express-session")
 const passport = require("passport")
-const LocalStrategy = require("passport-local")
-const db = require("mongodb")
-const ObjectID = require("mongodb").ObjectID
-const bcrypt = require("bcrypt")
-
-// const ensureAuthenticated = require("./middleware/isAuthenticated")
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect("/")
-}
+const routes = require("./routes")
+const auth = require("./auth")
 
 app.set("view engine", "pug")
 
@@ -41,98 +31,14 @@ app.use(passport.session())
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users")
 
-  // Be sure to change the title
-  app.route("/").get((req, res) => {
-    //Change the response to render the Pug template
-    res.render("pug", {
-      title: "Connected to Database",
-      message: "Please login",
-      showLogin: true,
-      showRegistration: true,
-    })
-  })
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id)
-  })
-
-  passport.deserializeUser((id, done) => {
-    db.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc)
-    })
-  })
-
-  passport.use(
-    new LocalStrategy(function (username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log("User " + username + " attempted to log in.")
-        if (err) {
-          return done(err)
-        }
-        if (!user) {
-          return done(null, false)
-        }
-        if (!bcrypt.compareSync(password, user.password)) {
-          return done(null, false)
-        }
-        return done(null, user)
-      })
-    })
-  )
+  routes(app, myDataBase)
+  auth(app, myDataBase)
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" })
   })
 })
 
-app.route("/register").post(
-  (req, res, next) => {
-    myDataBase.findOne({ username: req.body.username }, function (err, user) {
-      if (err) {
-        next(err)
-      } else if (user) {
-        res.redirect("/")
-      } else {
-        const hash = bcrypt.hashSync(req.body.password, 12)
-
-        myDataBase.insertOne(
-          {
-            username: req.body.username,
-            password: hash,
-          },
-          (err, doc) => {
-            if (err) {
-              res.redirect("/")
-            } else {
-              // The inserted document is held within
-              // the ops property of the doc
-              next(null, doc.ops[0])
-            }
-          }
-        )
-      }
-    })
-  },
-  passport.authenticate("local", { failureRedirect: "/" }),
-  (req, res, next) => {
-    res.redirect("/profile")
-  }
-)
-
-app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  res.render(process.cwd() + "/views/pug/profile", { username: req.user.username })
-})
-
-app.post("/login", passport.authenticate("local", { failureRedirect: "/" }), async (req, res) => {
-  res.redirect("/profile").render("pug")
-})
-
-app.route("/logout").get((req, res) => {
-  req.logout()
-  res.redirect("/")
-})
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log("Listening on port " + PORT)
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + process.env.PORT)
 })
