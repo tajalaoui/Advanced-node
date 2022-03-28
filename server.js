79ea0817a1f98fd312200b29c2aac881ed707d4a
Tajeddine <tajeddine.js@hotmail.com>
2022-03-28 10:37:49 +0000

    Added middleware to ensure user is authenticated

--- server.js
+++ server.js
@@ -79,6 +79,17 @@ myDB(async (client) => {
   })
 })
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect("/")
+}
+
+app.route("/profile").get(ensureAuthenticated, (req, res) => {
+  res.render(process.cwd() + "/views/pug/profile")
+})
+
 app.post("/login", passport.authenticate("local", { failureRedirect: "/" }), async (req, res) => {
   res.redirect("/profile").render("pug")
 })
